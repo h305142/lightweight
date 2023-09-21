3598bb6ad04084ad80a2bafd4d8269b9b747f3c7
h305142 <hjw0330@foxmail.com>
2023-09-21 19:55:36 +0800

    Delete Sum_dist.m

--- Sum_dist.m
+++
@@ -1,8 +0,0 @@
-function [sum_dis, pred_label, pred_otlIdx] = Sum_dist(centers, data, z)
-% calculate the k-means objective value with z outliers. 
-    D = pdist2(data,centers,'euclidean');
-    [dist,pred_label] = min(D,[],2);
-    [~,pred_otlIdx] = maxk(dist,z); 
-    dist(pred_otlIdx,:) = [];
-    sum_dis = sum(dist);
-end
